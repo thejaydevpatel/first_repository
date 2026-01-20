8236e4e3e9266e9804cee47777c8aed157721bdc
jaydev patel <pateljaydev22333@gmail.com>
2026-01-20 15:28:31 +0530

    initial cummit

---
+++ monday/index.js
@@ -0,0 +1,14 @@
+let n = 4;
+let num = 1;
+let pt = "";
+
+for( let i = 1 ; i <= n ; i++){
+    for(let j = 1; j <= n; j++){
+        pt += num + " ";
+        num++;
+        console.log(i);
+    }
+    pt += "\n";
+    console.log("\n");
+    
+}
